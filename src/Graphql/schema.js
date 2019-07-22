c9e44c636a82fb13e2500482edfed39fca17ed32
ali <alii25@barac.io>
2019-07-22 13:05:35 +0200

    delete schema

--- src/Graphql/schema.js
+++
@@ -1,9 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools';
-import { typeDefs, resolvers } from './createDynamicSchema';
-
-
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
-export default schema;
